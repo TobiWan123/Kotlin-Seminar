5cdb99b9aa9b66a4599b529612be863f5edf4945
Walter, Tobias <69347097+TobiWan123@users.noreply.github.com>
2021-09-30 19:47:11 +0200

    initial

---
+++ src/Main.kt
@@ -0,0 +1,32 @@
+fun main() {
+    /** String Templates **/
+
+    /** Einzeilige Funktionen **/
+
+    /** Extension **/
+
+    /** TypeCasts **/
+
+    /** Destructuring **/
+
+    /** Maybe-Typen **/
+
+    /** if not null **/
+
+    /** Zuweisungen mit if-Abfragen **/
+
+    /** for-schleife mit Indizes **/
+
+    /** When **/
+
+    /** lateinit **/
+
+}
+
+/** Data Classes **/
+
+/** Sekundäre Konstruktoren	**/
+
+/** Getter und Setter **/
+
+/** Init-Blöcke **/
